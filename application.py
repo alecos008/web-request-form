b13a962f16e46c6560c998c9dc087518f854c59d
alecos008 <acazorla72@gmail.com>
2020-05-02 21:23:37 +0200

    introduced a variable

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
 
+form = "form-control"
 @app.route("/index.html")
 def index ():
     return render_template ("index.html")
@@ -16,9 +17,13 @@ def appoint ():
 
 @app.route("/scheduled.html", methods=["POST"])
 def scheduled ():
+    form =  request.form.get ("form-control")
     name = request.form.get ("name")
     date = request.form.get ("date")
     email = request.form.get ("email")
-        
+
+  
     return render_template ("scheduled.html", name=name, date=date, email=email)
+    
+
     
